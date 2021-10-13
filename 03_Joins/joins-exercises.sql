bcbdb31ee0d03d905f2c8540c06f5eb3a6562dd0
Jeff Dean <jddean@umich.edu>
2021-10-13 16:50:55 -0400

    Jeff coding 1-5

--- 03_Joins/joins-exercises.sql
+++ 03_Joins/joins-exercises.sql
@@ -3,18 +3,59 @@
 
 -- 1. The city name, country name, and city population of all cities in Europe with population greater than 1 million
 -- (36 rows)
+      SELECT city.name, city.population, city.countrycode, country.name
+      FROM city 
+      JOIN country
+      ON country.code = city.countrycode
+      WHERE city.population >= 1000000
+      AND continent = 'Europe';
+      
+   
 
 -- 2. The city name, country name, and city population of all cities in countries where French is an official language and the city population is greater than 1 million
 -- (2 rows)
-
+       SELECT city.name, city.population,city.countrycode,country.name,countrylanguage.countrycode, countrylanguage.language
+       FROM city
+       JOIN country
+       ON country.code = city.countrycode
+       JOIN countrylanguage
+       ON countrylanguage.countrycode = city.countrycode
+       WHERE countrylanguage.language = 'French'
+       AND isofficial = true
+       AND city.population > 1000000;
+        
 -- 3. The name of the countries and continents where the language Javanese is spoken
 -- (1 row)
 
+       SELECT country.name, country.code, country.continent, countrylanguage.countrycode, countrylanguage.language
+       FROM country
+       JOIN countrylanguage
+       ON country.code = countrylanguage.countrycode
+       WHERE countrylanguage.language = 'Javanese';
+
 -- 4. The names of all of the countries in Africa that speak French as an official language
 -- (5 row)
+        SELECT country.name, country.code, country.continent, countrylanguage.countrycode, countrylanguage.language, countrylanguage.isofficial
+        FROM country
+        JOIN countrylanguage
+        ON country.code = countrylanguage.countrycode
+        WHERE country.continent = 'Africa'
+        AND countrylanguage.language = 'French'
+        AND countrylanguage.isofficial = 'true';
+        
 
 -- 5. The average city population of cities in Europe
 -- (average city population in Europe: 287,684)
+
+       SELECT avg(city.population) as avgpop
+       FROM country
+       JOIN city 
+       ON country.code = city.countrycode
+       WHERE country.continent = 'Europe'
+       
+      
+       
+       
 
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
