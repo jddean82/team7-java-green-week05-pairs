8d44e15cf0f5f9e8e159f0c13e004d67f77d323f
Becky Plas <becky.plas@gmail.com>
2021-10-16 14:14:49 -0400

    Finished

--- 05_Database_Design/AnimalHospital.sql
+++ 05_Database_Design/AnimalHospital.sql
@@ -1,3 +1,12 @@
+--We designed the database with the following assumptions: 
+--(1) There is only one hospital location and address
+--(2) There is one invoice generated per visit
+--(3) Owners with multiple pets will book separate visits for each pet
+--(4) Each pet has one owner
+--(5) The same procedure will not be carried out multiple times on a pet on the same day.
+
+
+DROP TABLE IF EXISTS VisitPetProcedure;
 DROP TABLE IF EXISTS Visit;
 DROP TABLE IF EXISTS Pet;
 DROP TABLE IF EXISTS Owner ;
@@ -15,7 +24,6 @@ OwnerZip varchar(10) NOT NULL,
 OwnerState varchar(15),
 
 CONSTRAINT pk_Owner PRIMARY KEY(OwnerID)
-
 
 );
 
@@ -48,15 +56,26 @@ CONSTRAINT fk_OwnerId FOREIGN KEY (OwnerID) REFERENCES Owner(OwnerID)
 
 CREATE TABLE Visit
 (
-
 VisitID serial,
+VisitDate date NOT NULL,
+VisitDescription varchar(128),
+
+CONSTRAINT pk_Visit PRIMARY KEY (VisitID)
+
+);
+
+
+CREATE TABLE VisitPetProcedure 
+(
+
+VisitID int NOT NULL,
 ProcedureID int NOT NULL,
 PetID int NOT NULL,
-VisitDate date NOT NULL,
 
-CONSTRAINT pk_VisitID PRIMARY KEY(VisitID),
-CONSTRAINT fk_Visit_ProcedureID FOREIGN KEY(ProcedureID) REFERENCES Procedure(ProcedureID) ,
-CONSTRAINT fk_PetID FOREIGN KEY(PetID) REFERENCES Pet(PetID)
+CONSTRAINT pk_VisitID PRIMARY KEY(VisitID, PetID, ProcedureID),
+CONSTRAINT fk_Visit_ProcedureID FOREIGN KEY(ProcedureID) REFERENCES Procedure(ProcedureID),
+CONSTRAINT fk_PetID FOREIGN KEY(PetID) REFERENCES Pet(PetID),
+CONSTRAINT fk_VisitID FOREIGN KEY (VisitID) REFERENCES Visit(VisitID)
 
 );
 --------------------------------------------------------------------------------------------------DATA LOAD------------------------------------------
@@ -69,3 +88,14 @@ INSERT INTO Pet(OwnerID,PetName, PetType,PetAge) Values(1,'Henry','Dog',1);
 INSERT INTO Pet(OwnerID,PetName, PetType,PetAge) Values(2,'MrBill','Donkey',12);
 INSERT INTO Pet(OwnerID,PetName, PetType,PetAge) Values(3,'Whiskery','Bird',10);
 
+INSERT INTO Procedure(ProcedureName, ProcedureCost) VALUES('Whisker check', 900);
+INSERT INTO Procedure(ProcedureName, ProcedureCost) VALUES('Flea treatment', 100);
+INSERT INTO Procedure(ProcedureName, ProcedureCost) VALUES('Deworming', 300);
+
+INSERT INTO Visit(VisitDate, VisitDescription) VALUES ('2021-10-16', 'Standard kitty checkup');
+INSERT INTO Visit(VisitDate, VisitDescription) VALUES ('2021-4-12', 'Annual exam');
+INSERT INTO Visit(VisitDate, VisitDescription) VALUES ('2021-10-18', 'Whisker count');
+
+INSERT INTO VisitPetProcedure(VisitID, PetId, ProcedureId) VALUES(3, 2, 1);
+INSERT INTO VisitPetProcedure(VisitID, PetId, ProcedureId) VALUES(2, 3, 1);
+INSERT INTO VisitPetProcedure(VisitID, PetId, ProcedureId) VALUES(1, 2, 1);
